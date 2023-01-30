15b22f409a1fa2d75cbb80eb1e428e353c35fa6a
AllBill127 <90201218+AllBill127@users.noreply.github.com>
2023-01-30 13:41:19 +0200

    Add Branch class

---
+++ Tree_depth_calculation/Branch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Tree_depth_calculation
+{
+    class Branch
+    {
+        List<Branch> branches;
+
+        public Branch()
+        {
+            branches = new List<Branch>();
+        }
+
+        public void AddBranch(Branch branch)
+        {
+            branches.Add(branch);
+        }
+        public void RemoveBranch(Branch branch)
+        {
+            branches.Remove(branch);
+        }
+        public List<Branch> GetBranches()
+        {
+            return branches;
+        }
+    }
+}
